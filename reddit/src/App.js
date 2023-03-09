18cd1886e858368a2d4e67069d5e3451c23d4bec
kalash jain <kalashjain54@gmail.com>
2023-03-10 00:39:42 +0530

    adding icons

--- reddit/src/App.js
+++ reddit/src/App.js
@@ -1,9 +1,20 @@
+import Logo from "./logo.png";
+import "./index.css";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <header className="flex bg-reddit_dark p-2">
+        <div className="mx-4">
+          <img src={Logo} alt="/" className=" w-8 h-8 " />
+        </div>
+        <form action="" className="bg-gray-800 p-1 flex">
+          <SearchIcon className="bg-gray-800 h-8" />
+          <input type="text" className="bg-gray-800 h-8 text-white"></input>
+        </form>
+      </header>
+    </div>
   );
 }
 
--- reddit/tailwind.config.js
+++ reddit/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        reddit_dark:{
+          DEFAULT: '#030303',
+        }
+      }
+    },
   },
   plugins: [],
 }
